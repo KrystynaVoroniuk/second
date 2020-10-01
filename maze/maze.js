d00bdca1b631fb9c8249dc7b04dc6684126d8f2a
Krystyna <krystyna.voroniuk@nure.ua>
2020-10-01 19:22:40 +0300

    second

--- maze/maze.js
+++ maze/maze.js
@@ -16,33 +16,33 @@ let gen = new Generator(10, 10);
 console.log(gen.matrix); 
   
  
-function maze(gen) { 
+// function maze(gen) { 
  
-  for (let item of gen.matrix) { 
-    for (let cell of item) { 
-      if (cell === 1) { 
-        let div = document.createElement('div'); 
-        div.className = "canal"; 
-        document.body.append(div); 
-      } else if (cell === 0) { 
-        let div1 = document.createElement('div'); 
-        div1.className = "wall"; 
-        document.body.append(div1); 
-      } else if (cell === 2) { 
-        let div2 = document.createElement('div'); 
-        div2.className = "entry";
-        div2.innerHTML = "<strong>Вход</strong>"; 
-        document.body.append(div2); 
-      } else { 
-        let div3 = document.createElement('div'); 
-        div3.className = "exit";
-        div3.innerHTML = "<strong>Выход</strong>"; 
-        document.body.append(div3); 
-      } 
-    } 
+//   for (let item of gen.matrix) { 
+//     for (let cell of item) { 
+//       if (cell === 1) { 
+//         let div = document.createElement('div'); 
+//         div.className = "canal"; 
+//         document.body.append(div); 
+//       } else if (cell === 0) { 
+//         let div1 = document.createElement('div'); 
+//         div1.className = "wall"; 
+//         document.body.append(div1); 
+//       } else if (cell === 2) { 
+//         let div2 = document.createElement('div'); 
+//         div2.className = "entry";
+//         div2.innerHTML = "<strong>Вход</strong>"; 
+//         document.body.append(div2); 
+//       } else { 
+//         let div3 = document.createElement('div'); 
+//         div3.className = "exit";
+//         div3.innerHTML = "<strong>Выход</strong>"; 
+//         document.body.append(div3); 
+//       } 
+//     } 
  
  
  
-  } 
-} 
-maze(gen);
+//   } 
+// } 
+// maze(gen);
